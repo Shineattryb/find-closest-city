f6e4a696908dc3098607dffb90a69fa2c0f847ad
shine_52 <shine@attryb.com>
2024-07-29 19:10:41 +0530

    changes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //to be fetched from visitorinfo
 let city='Thane';
-let citylatitude='19.2183';
-let citylongitude='72.9783';
+let citylatitude='19.21833';
+let citylongitude='72.97808';
 
 const tierOneCities=[
-   {cityName: 'Bangalore', latitude: 12.9716, longitude: 77.5946},
-   {cityName: 'Mumbai',latitude: 19.0760, longitude: 72.8777},
-  { cityName: 'Chennai',latitude: 13.0827, longitude: 80.2707},
-   {cityName:'Delhi',latitude: 28.7041, longitude: 77.1025},
-   {cityName: 'Hyderabad',latitude: 17.3850, longitude: 78.4867},
-  { cityName: 'Kolkata',latitude: 22.5726, longitude: 88.3639},
-   {cityName: 'Ahmedabad',latitude: 23.0225, longitude: 72.5714},
-   {cityName: 'Pune',latitude: 18.5204, longitude: 73.8567}
+   {cityName: 'Bangalore', latitude: 12.97164, longitude: 77.59460},
+   {cityName: 'Mumbai',latitude: 19.07600, longitude: 72.87770},
+  {cityName: 'Chennai',latitude: 13.08268, longitude: 80.27072},
+   {cityName:'Delhi',latitude: 28.62739, longitude: 77.17169},
+   {cityName: 'Hyderabad',latitude: 17.38504, longitude: 78.48667},
+  { cityName: 'Kolkata',latitude: 22.57264, longitude: 88.36389},
+   {cityName: 'Ahmedabad',latitude: 23.02250, longitude: 72.57136},
+   {cityName: 'Pune',latitude: 18.52043, longitude: 73.85674}
 ];
 function deg2rad(deg) {
  return deg*(Math.PI/180)
@@ -19,25 +19,30 @@ function deg2rad(deg) {
 
 //use haversine distance to calaculate distance between cities(using longitude and latitude)
 let R=6371; //Radius of earth in km
-function haversineDistance( tierOneLatitude, tierOneLongitude){
+function haversineDistance( tierOneLatitude, tierOneLongitude,citylatitude,citylongitude){
+      // Calculate differences in latitude and longitude
     var latitudeDiff= deg2rad(tierOneLatitude-citylatitude);
     var longitudeDiff=deg2rad(tierOneLongitude-citylongitude);
     const dist=2*(R) * Math.asin(Math.sqrt(Math.sin(latitudeDiff/2) ** 2 + Math.cos(deg2rad(citylatitude))* Math.cos(deg2rad(tierOneLatitude)) * Math.sin(longitudeDiff/2)** 2 ))
-   console.log(dist);
+  
     return dist; //in km
 }
-
+// Function to find the closest city within a threshold distance
 function findClosestCity(){
     let thresholdDistance=25; //in km
-    let ClosestCity=null;
-    for(const city of tierOneCities){
-        const distance=haversineDistance(city.latitude,city.longitude);
-        if(distance<=thresholdDistance){
-            thresholdDistance=distance;
-            ClosestCity=city;
-        }
-    }
-    return ClosestCity;
+  
+    const citiesWithDistance = tierOneCities.map(tierOneCity => {
+        const distance = haversineDistance(tierOneCity.latitude, tierOneCity.longitude,citylatitude,citylongitude);
+        return {
+            city: tierOneCity.cityName,
+            distance, 
+            isWithinThreshold: distance <= thresholdDistance,
+            distanceMeasuredFromCity:city
+        };
+    });
+
+    return citiesWithDistance; 
+    
 }
-const closestCity = findClosestCity();
-console.log(closestCity);
+const citiesWithDistance = findClosestCity();
+console.log(citiesWithDistance);
